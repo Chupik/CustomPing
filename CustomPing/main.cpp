c6b96e10840061a7dccf70dde63259e2fcaaf872
chupik <Chupik@MacBook-Air-Alexandr.local>
2015-04-10 12:40:02 +0300

    Initial Commit

---
+++ CustomPing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CustomPing
+//
+//  Created by Alexandr on 10.04.15.
+//  Copyright (c) 2015 Alexandr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
